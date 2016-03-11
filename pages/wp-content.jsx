87409f7ced73080caaaf2c01e17f5697dcdd36bd
Mavis Ou <mmmavisss@gmail.com>
2016-03-10 21:02:33 -0800

    forgot to add wp-content.jsx

---
+++ pages/wp-content.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var Page = require('../components/page.jsx');
+var WpContentLoader = require('react-wp-content-loader');
+
+// [TODO] 404 needs to be handled properly. Now we just make all the 
+//        non-component pages as wp-pages
+var WpContent = React.createClass({
+  render: function() {
+    return (
+      <WpContentLoader wpUrl="teachmozillaorg.wordpress.com" wpPostSlug={this.props.params.wpSlug} />
+    );
+  }
+});
+
+module.exports = WpContent;
