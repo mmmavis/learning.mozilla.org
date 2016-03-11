015a7c8d5533a2e45dcbb2b61e9b8566ca281d95
Mavis Ou <mmmavisss@gmail.com>
2016-03-11 10:50:00 -0800

    code cleanup

--- pages/wp-content.jsx
+++ pages/wp-content.jsx
@@ -1,5 +1,4 @@
 var React = require('react');
-var Page = require('../components/page.jsx');
 var WpContentLoader = require('react-wp-content-loader');
 
 // [TODO] 404 needs to be handled properly. Now we just make all the 
